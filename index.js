2f74845f334d0c4b5a9fd651597ba440a5aa6a83
Mohammad Reza Enteshari <reza.ent@hotmail.com>
2023-03-08 00:01:09 -0500

    Adds two files

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const countDown =(num) =>
+{
+while(num>=0){
+console.log(num);
+num--;
+}
+}
+countDown();
+
+const names = ["Guadalupe", "Ollie", "Aki"]
+
+function writeCards(names) {
+   return  names.map(name=> `Thank you, ${name}, for the wonderful surprise gift!`)
+}
+
+console.log(writeCards(names, "birthday"))
+
+//"Thank you, Guadalupe, for the wonderful surprise gift!"
